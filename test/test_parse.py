90e34e976f6cc257c8992701872a3a2f60424bd7
Emre Sahin <github@emresult.com>
2021-03-31 10:14:52 +0300

    Added new test for katacoda-tagged code blocks

--- test/test_parse.py
+++ test/test_parse.py
@@ -10,7 +10,7 @@ class ParseTestCase(unittest.TestCase):
         tests = [("`print`",
                   [CodeBlock(code="print",
                             start_pos=0,
-                            end_pos=6,
+                            end_pos=7,
                             delim="`",
                             language="",
                             katacoda_tag="")]
@@ -25,7 +25,7 @@ class ParseTestCase(unittest.TestCase):
         tests = [("`print`",
                   [CodeBlock(code="print",
                             start_pos=0,
-                            end_pos=6,
+                            end_pos=7,
                             delim="`",
                             language="",
                             katacoda_tag="")]
@@ -35,10 +35,44 @@ class ParseTestCase(unittest.TestCase):
             self.assertEqual(parse.parse_text(md_text),
                              code_block_list,
                              f"Test {i} fails:\n{md_text}\n{code_block_list}")
+
+    def test_parse_code_block_with_katacoda(self):
+        tests = [("""
+`print`{{execute}}
+
+and also
+
+```python
+   print(x, y)
+```{{execute}}
+""", 
+                  ["{{execute}}"],
+                  [CodeBlock(code="print",
+                             start_pos=0,
+                             end_pos=0,
+                             delim="`",
+                             language="",
+                             katacoda_tag="{{execute}}"),
+                   CodeBlock(code="print(x,y)",
+                             start_pos=0,
+                             end_pos=0,
+                             delim="`",
+                             language="python",
+                             katacoda_tag="{{execute}}")])]
+
+
+        for i, (md_text, katacoda_tags, code_block_list) in enumerate(tests):
+            self.assertEqual(parse.parse_text(md_text, katacoda_tags=katacoda_tags),
+                             code_block_list,
+                             f"Test {i} fails:\n{md_text}\n{code_block_list}")
+
+
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(ParseTestCase('test_search_inline_code'))
     suite.addTest(ParseTestCase('test_parse_code_without_katacoda'))
+    suite.addTest(ParseTestCase('test_parse_code_with_katacoda'))
+
     return suite
 
 if __name__ == '__main__':
