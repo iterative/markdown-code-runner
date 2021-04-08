700a4d70441a4e54df7f0763d40b2ff597858a6c
Emre Sahin <github@emresult.com>
2021-04-08 09:45:23 +0000

    Added --debug option to run-katacoda-docs

--- test/run-katacoda-docs.zsh
+++ test/run-katacoda-docs.zsh
@@ -5,25 +5,20 @@
 # containers after cloning the katacoda-scenarios repository. 
 
 
-REPO=https://github.com/iesahin/katacoda-scenarios
+REPO=https://github.com/iterative/katacoda-scenarios
 
 CLONE_PATH="/tmp/katacoda-scenarios-${RANDOM}"
 
-DIR="${CLONE_PATH}/get-started-dockerized"
+DIR="${CLONE_PATH}/get-started"
 export PYTHONPATH=${PYTHONPATH}:$(dirname $0)/../
-RUNNER="$(dirname $0)/../bin/run-in-container.py"
+RUNNER="$(dirname $0)/../bin/run-in-container.py" 
 
 git clone $REPO $CLONE_PATH
 
-$RUNNER -c emresult/katacoda-base -k execute ${DIR}/01-initialize/*.md 
-
-$RUNNER -c emresult/katacoda-gs-versioning -k execute ${DIR}/02-versioning/*.md
-
-$RUNNER -c emresult/katacoda-gs-accessing -k execute ${DIR}/03-accessing/*.md
-
-$RUNNER -c emresult/katacoda-gs-stages -k execute ${DIR}/04-stages/*.md
-
-$RUNNER -c emresult/katacoda-gs-params -k execute ${DIR}/05-params-metrics-plots/*.md
-
-$RUNNER -c emresult/katacoda-gs-experiments -k execute ${DIR}/06-experiments/*.md
+$RUNNER --debug -c emresult/doc-katacoda:base -k execute ${DIR}/01-initialize/*.md 
+$RUNNER --debug -c emresult/doc-katacoda-gs:versioning -k execute ${DIR}/02-versioning/*.md
+$RUNNER --debug -c emresult/doc-katacoda-gs:accessing -k execute ${DIR}/03-accessing/*.md
+$RUNNER --debug -c emresult/doc-katacoda-gs:stages -k execute ${DIR}/04-stages/*.md
+$RUNNER --debug -c emresult/doc-katacoda-gs:params -k execute ${DIR}/05-params-metrics-plots/*.md
+$RUNNER --debug -c emresult/doc-katacoda-gs:experiments -k execute ${DIR}/06-experiments/*.md
 
